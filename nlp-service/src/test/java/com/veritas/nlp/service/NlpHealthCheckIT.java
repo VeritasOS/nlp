f69c41ca85bed647a14d29ea1a07771f252dcaff
Sandeep Kulkarni <Sandeep.Kulkarni4@veritas.com>
2021-07-07 19:36:11 +0530

    Added unit tests for NlpHealthCheck class
    The current test class NlpHealthCheckTest is actually an IT test class. We need to write unit tests for this class. We will rename this class to NlpHealthCheckIT. Also added Mockito as test dependency.

---
+++ nlp-service/src/test/java/com/veritas/nlp/service/NlpHealthCheckIT.java
@@ -0,0 +1,21 @@
+package com.veritas.nlp.service;
+
+import com.veritas.nlp.ner.NerSettings;
+import com.veritas.nlp.resources.NerResource;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class NlpHealthCheckIT {
+
+    @Test
+    public void checkSucceeds() throws Exception {
+        NlpServiceSettings nlpServiceSettings = new NlpServiceSettings();
+        nlpServiceSettings.setNerSettings(new NerSettings());
+
+        NlpHealthCheck check = new NlpHealthCheck(new NerResource(nlpServiceSettings));
+
+        assertThat(check.check().isHealthy()).isTrue();
+    }
+}
